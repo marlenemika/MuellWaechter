06c54871575c769aedc0da65d232b8d9c4608d53
Marlene Mika <marlene.mika@uni-ulm.de>
2023-03-29 23:39:45 +0200

    feat: import missing class content view

--- MuellWaechter/MuellWaechter/ContentView.swift
+++ MuellWaechter/MuellWaechter/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var model = DataModel()
+    
+    private static let barHeightFactor = 0.10
+    
+    @State private var isImageTaken = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationView {
+            GeometryReader { geometry in
+                ViewfinderView(image: $model.viewfinderImage)
+                    .overlay(alignment: .top) {
+                        buttonsView()
+                            .frame(height: geometry.size.height * (Self.barHeightFactor+0.075))
+                            .background(.black.opacity(0.7))
+                    }
+                    .overlay(alignment: .bottom) {
+                        Text("Test").foregroundColor(.pink)
+                            .frame(height: geometry.size.height * (Self.barHeightFactor+0.075))
+                            .background(.black.opacity(0.7))
+                    }
+                    .background(.black)
+            }
+            .task {
+                await model.camera.start()
+            }
+            .navigationTitle("Camera")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+            .ignoresSafeArea()
+            .statusBar(hidden: true)
+            .onAppear(){
+                UIApplication.shared.isIdleTimerDisabled = true
+            }.onDisappear(){
+                UIApplication.shared.isIdleTimerDisabled = false
+            }
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    private func buttonsView() -> some View {
+        HStack() {
+            Spacer()
+            Button {
+                model.camera.switchCaptureDevice()
+            } label: {
+                Label("Switch Camera", systemImage: "arrow.triangle.2.circlepath")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundColor(.white)
+            }.frame(maxWidth: .leastNormalMagnitude, alignment: .trailing)
+        }
+        .buttonStyle(.plain)
+        .labelStyle(.iconOnly)
+        .padding()
     }
 }
