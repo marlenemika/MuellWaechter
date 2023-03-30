3ab335c974746fa350d4fcfeea062a2674c4c140
Marlene Mika <marlene.mika@uni-ulm.de>
2023-03-30 03:23:32 +0200

    feat: switch camera device button and UI

--- MuellWaechter/MuellWaechter/ContentView.swift
+++ MuellWaechter/MuellWaechter/ContentView.swift
@@ -17,50 +17,42 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-            GeometryReader { geometry in
+            ZStack() {
                 ViewfinderView(image: $model.viewfinderImage)
                     .overlay(alignment: .top) {
-                        buttonsView()
-                            .frame(height: geometry.size.height * (Self.barHeightFactor+0.075))
-                            .background(.black.opacity(0.7))
+                        Rectangle()
+                            .size(width: 500, height: 150)
+                            .fill(Color(.black).opacity(0.7))
                     }
-                    .overlay(alignment: .bottom) {
-                        Text("Test").foregroundColor(.pink)
-                            .frame(height: geometry.size.height * (Self.barHeightFactor+0.075))
-                            .background(.black.opacity(0.7))
+                VStack() {
+                    Spacer()
+                    Text("Test")
+                        .foregroundColor(.pink)
+                        .background(.black.opacity(0.7))
+                }
+                .toolbar(content: {
+                    Button {
+                        model.camera.switchCaptureDevice()
+                    } label: {
+                        Label("Switch Camera", systemImage: "arrow.triangle.2.circlepath")
+                            .font(.system(size: 36, weight: .bold))
+                            .foregroundColor(.white)
                     }
-                    .background(.black)
+                        .buttonStyle(.plain)
+                        .labelStyle(.iconOnly)
+                })
             }
             .task {
                 await model.camera.start()
             }
-            .navigationTitle("Camera")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarHidden(true)
             .ignoresSafeArea()
-            .statusBar(hidden: true)
+            .navigationTitle("Live Ansicht")
+            .navigationBarTitleDisplayMode(.inline)
             .onAppear(){
                 UIApplication.shared.isIdleTimerDisabled = true
             }.onDisappear(){
                 UIApplication.shared.isIdleTimerDisabled = false
             }
-            
         }
-    }
-    
-    private func buttonsView() -> some View {
-        HStack() {
-            Spacer()
-            Button {
-                model.camera.switchCaptureDevice()
-            } label: {
-                Label("Switch Camera", systemImage: "arrow.triangle.2.circlepath")
-                    .font(.system(size: 36, weight: .bold))
-                    .foregroundColor(.white)
-            }.frame(maxWidth: .leastNormalMagnitude, alignment: .trailing)
-        }
-        .buttonStyle(.plain)
-        .labelStyle(.iconOnly)
-        .padding()
     }
 }
