e0d777dcb10041d2dcb96241284713f0ef88ef0c
Marlene Mika <marlene.mika@uni-ulm.de>
2023-07-30 22:34:43 +0200

    collapsible

---
+++ MuellWaechter/MuellWaechter/Collapsible.swift
@@ -0,0 +1,39 @@
+//
+//  Collapsible.swift
+//  MüllWächter
+//
+//  Created by Marlene Mika on 30.07.23.
+//
+
+import SwiftUI
+
+struct Collapsible<Content: View>: View {
+    @State var label: () -> Text
+    @State var content: () -> Content
+    
+    @State private var collapsed: Bool = true
+    
+    var body: some View {
+        VStack {
+            Button(
+                action: { self.collapsed.toggle() },
+                label: {
+                    HStack {
+                        self.label()
+                        Spacer()
+                        Image(systemName: self.collapsed ? "chevron.down" : "chevron.up")
+                    }
+                }
+            )
+            .buttonStyle(BorderedButtonStyle())
+            
+            VStack {
+                self.content()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: collapsed ? 0 : .none)
+            .clipped()
+            .animation(.easeOut)
+            .transition(.slide)
+        }
+    }
+}
